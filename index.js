7141f84475ddbd7d473e98e3bed9bfeaa4223e8d
ZikaZaki <zika.github@gmail.com>
2022-04-01 04:09:49 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,24 +2,25 @@ require("isomorphic-unfetch");
 const { promises: fs } = require("fs");
 const path = require("path");
 
-
 async function main() {
-    const readmeTemplate = (
-        await fs.readFile(path.join(process.cwd(), "./README.template.md"))
-    ).toString("utf-8");
+  const readmeTemplate = (
+    await fs.readFile(path.join(process.cwd(), "./README.template.md"))
+  ).toString("utf-8");
 
-    const office_quote = await (
-        await fetch("https://officeapi.dev/api/quotes/random")
-    ).json();
+  const office_quote = await (
+    await fetch("https://officeapi.dev/api/quotes/random")
+  ).json();
 
-    console.log(office_quote);
+  console.log(office_quote);
 
+  const readme = readmeTemplate
+    .replace("{office_quote}", office_quote.data.content)
+    .replace(
+      "{office_character}",
+      `- ${office_quote.data.character.firstname} ${office_quote.data.character.lastname}`
+    );
 
-    const readme = readmeTemplate
-        .replace("{office_quote}", office_quote.data.content)
-        .replace("{office_character}", `- ${office_quote.data.character.firstname} ${office_quote.data.character.lastname}`)
-
-    await fs.writeFile("README.md", readme);
+  await fs.writeFile("README.md", readme);
 }
 
 main();
